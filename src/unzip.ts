a2670e8d9ffbdffe2ee93c34fdb8a7b95fd72642
Nathan Rajlich <nathan@tootallnate.net>
2019-02-09 15:22:15 -0800

    Replace `adm-zip` with `yauzl`
    `adm-zip` was a poor choice, it doesn't handle errors at all leading to
    obscure error messages that I had to go digging into the source code to
    figure out what was going on.
    
    `yauzl` on the other hand is very strict aginst the spec and is the most
    popular / battle-tested unzip module on npm.

--- src/unzip.ts
+++ src/unzip.ts
@@ -1,10 +1,18 @@
-import { join } from 'path';
 import { tmpdir } from 'os';
+import * as Mode from 'stat-mode';
+import pipe from 'promisepipe';
 import createDebug from 'debug';
-import { promisify } from 'util';
-import * as AdmZip from 'adm-zip';
+import { dirname, basename, join } from 'path';
+import { createWriteStream, mkdirp, symlink } from 'fs-extra';
+import * as streamToPromise from 'stream-to-promise';
+import {
+	Entry,
+	ZipFile,
+	open as zipFromFile,
+	fromBuffer as zipFromBuffer
+} from 'yauzl-promise';
 
-const debug = createDebug('@zeit/fun:index');
+const debug = createDebug('@zeit/fun:unzip');
 
 export async function unzipToTemp(
 	data: Buffer | string,
@@ -16,9 +24,64 @@ export async function unzipToTemp(
 			.toString(16)
 			.substring(2)}`
 	);
-	debug('Unzipping %o to temp dir %o', data, dir);
-	const zip = new AdmZip(data);
-	await promisify(zip.extractAllToAsync)(dir, false);
-	debug('Done unzipping');
+	let zip: ZipFile;
+	if (Buffer.isBuffer(data)) {
+		debug('Unzipping buffer (length=%o) to temp dir %o', data.length, dir);
+		zip = await zipFromBuffer(data);
+	} else {
+		debug('Unzipping %o to temp dir %o', data, dir);
+		zip = await zipFromFile(data);
+	}
+	await unzip(zip, dir);
+	await zip.close();
+	debug('Finished unzipping to %o', dir);
 	return dir;
+}
+
+export async function* entries(zipFile: ZipFile) {
+	let entry: Entry;
+	while ((entry = await zipFile.readEntry()) !== null) {
+		yield entry;
+	}
+}
+
+const getMode = (entry: Entry) =>
+	new Mode({ mode: entry.externalFileAttributes >>> 16 });
+
+export async function unzip(zipFile: ZipFile, dir: string): Promise<void> {
+	for await (const entry of entries(zipFile)) {
+		const destPath = join(dir, entry.fileName);
+		if (/\/$/.test(entry.fileName)) {
+			debug('Creating directory %o', destPath);
+			await mkdirp(destPath);
+		} else {
+			const [entryStream] = await Promise.all([
+				entry.openReadStream(),
+				// ensure parent directory exists
+				mkdirp(dirname(destPath))
+			]);
+			const mode = getMode(entry);
+			if (mode.isSymbolicLink()) {
+				const linkDest = String(await streamToPromise(entryStream));
+				debug('Creating symboling link %o to %o', destPath, linkDest);
+				await symlink(linkDest, destPath);
+			} else {
+				const modeOctal = mode.toOctal();
+				debug(
+					'Unzipping file to %o with mode %o (o%s)',
+					destPath,
+					String(mode),
+					modeOctal
+				);
+				const destStream = createWriteStream(destPath, {
+					mode: parseInt(modeOctal, 8)
+				});
+				await pipe(
+					entryStream,
+					destStream
+				);
+				//debug('Finished unzipping file to %o', destPath);
+			}
+		}
+	}
 }
