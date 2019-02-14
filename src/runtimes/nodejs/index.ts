6e7167049cd7cab9e591ee94f2c2b4d83fabc6c2
Nathan Rajlich <nathan@tootallnate.net>
2019-02-13 21:07:37 -0800

    Fix `nodejs6.10` and `nodejs8.10` runtime init()
    By passing in a proper `runtimeDir` to the `nodejs.init()` passthrough
    function. This ensures that the function checks for the `bootstrap.js`
    file in the proper location.

--- src/runtimes/nodejs/index.ts
+++ src/runtimes/nodejs/index.ts
@@ -5,7 +5,7 @@ import { Runtime } from '../../types';
 
 const debug = createDebug('@zeit/fun:runtimes/nodejs');
 
-export async function init({ cacheDir, runtimeDir }: Runtime): Promise<void> {
+export async function init({ runtimeDir }: Runtime): Promise<void> {
 	const bootstrapPath = join(runtimeDir, 'bootstrap.js');
 	const bootstrapJs = await stat(bootstrapPath);
 	debug('Stat of %o: %o', bootstrapPath, bootstrapJs);
--- src/runtimes/nodejs8.10/index.ts
+++ src/runtimes/nodejs8.10/index.ts
@@ -1,8 +1,12 @@
+import { join } from 'path';
 import * as node from '../nodejs';
 import { Runtime } from '../../types';
 import { installNode } from '../../install-node';
 
 export async function init(runtime: Runtime): Promise<void> {
-	await node.init(runtime);
+	await node.init({
+		...runtime,
+		runtimeDir: join(runtime.runtimeDir, '../nodejs')
+	});
 	await installNode(runtime.cacheDir, '8.10.0');
 }
