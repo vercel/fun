b672ebb8a1550fed615cc0c883f3f31dedab6313
Nathan Rajlich <nathan@tootallnate.net>
2019-03-15 01:09:58 -0700

    Run `black` on Python runtime

--- src/runtimes/python/bootstrap.py
+++ src/runtimes/python/bootstrap.py
@@ -8,18 +8,26 @@ import importlib
 
 is_python_3 = sys.version_info > (3, 0)
 
-if (is_python_3):
+if is_python_3:
     import urllib.request
 else:
     import urllib2
+
 
 class LambdaRequest:
     def __init__(self, path, data=None):
         req = None
         runtime_path = '/2018-06-01/runtime/'
-        url = 'http://' + os.environ.get('AWS_LAMBDA_RUNTIME_API', '127.0.0.1:3000') + runtime_path + path
+        url = (
+            'http://'
+            + os.environ.get(
+                'AWS_LAMBDA_RUNTIME_API', '127.0.0.1:3000'
+            )
+            + runtime_path
+            + path
+        )
 
-        if (is_python_3):
+        if is_python_3:
             req = urllib.request.urlopen(url, data)
         else:
             req = urllib2.urlopen(url, data)
@@ -27,7 +35,7 @@ class LambdaRequest:
         info = req.info()
         body = req.read()
 
-        if (is_python_3):
+        if is_python_3:
             body = body.decode(encoding='UTF-8')
 
         self.status_code = req.getcode()
@@ -35,7 +43,7 @@ class LambdaRequest:
         self.info = info
 
     def get_header(self, name):
-        if (is_python_3):
+        if is_python_3:
             return self.info.get(name)
         else:
             return self.info.getheader(name)
@@ -43,14 +51,18 @@ class LambdaRequest:
     def get_json_body(self):
         return json.loads(self.body)
 
+
 def lambda_runtime_next_invocation():
     res = LambdaRequest('invocation/next')
 
-    if (res.status_code != 200):
-        raise Exception('Unexpected /invocation/next response: ' + res.body)
+    if res.status_code != 200:
+        raise Exception(
+            'Unexpected /invocation/next response: '
+            + res.body
+        )
 
     trace_id = res.get_header('Lambda-Runtime-Trace-Id')
-    if (trace_id is not None):
+    if trace_id is not None:
         os.environ['_X_AMZN_TRACE_ID'] = trace_id
     else:
         try:
@@ -69,40 +81,66 @@ def lambda_runtime_next_invocation():
 
     return (res.get_json_body(), context)
 
+
 def lambda_runtime_invoke_response(result, context):
-    body = json.dumps(result, separators=(',',':')).encode(encoding='UTF-8')
-    res = LambdaRequest('invocation/' + context['aws_request_id'] + '/response', body)
-    if (res.status_code != 202):
-        raise Exception('Unexpected /invocation/response response: ' + res.body)
+    body = json.dumps(result, separators=(',', ':')).encode(
+        encoding='UTF-8'
+    )
+    res = LambdaRequest(
+        'invocation/'
+        + context['aws_request_id']
+        + '/response',
+        body,
+    )
+    if res.status_code != 202:
+        raise Exception(
+            'Unexpected /invocation/response response: '
+            + res.body
+        )
+
 
 def lambda_runtime_invoke_error(err, context):
-    body = json.dumps(err, separators=(',',':')).encode(encoding='UTF-8')
-    res = LambdaRequest('invocation/' + context['aws_request_id'] + '/error', body)
+    body = json.dumps(err, separators=(',', ':')).encode(
+        encoding='UTF-8'
+    )
+    res = LambdaRequest(
+        'invocation/'
+        + context['aws_request_id']
+        + '/error',
+        body,
+    )
+
 
 def lambda_runtime_get_handler():
     mod = importlib.import_module('handler')
     # TODO: invoke `__init__`?
     return getattr(mod, 'handler')
 
+
 def lambda_runtime_main():
     sys.path.insert(0, os.environ['PWD'])
-    sys.path.insert(0, os.environ.get('LAMBDA_TASK_ROOT', '/var/task'))
+    sys.path.insert(
+        0, os.environ.get('LAMBDA_TASK_ROOT', '/var/task')
+    )
 
     fn = lambda_runtime_get_handler()
 
-    while (True):
+    while True:
         (event, context) = lambda_runtime_next_invocation()
-        #print(event)
-        #print(context)
+        # print(event)
+        # print(context)
         result = None
         try:
             result = fn(event, context)
         except:
             err = str(sys.exc_info()[0])
             print(err)
-            lambda_runtime_invoke_error({ 'error': err }, context)
+            lambda_runtime_invoke_error(
+                {'error': err}, context
+            )
         else:
             lambda_runtime_invoke_response(result, context)
 
+
 if __name__ == '__main__':
-  lambda_runtime_main()
+    lambda_runtime_main()
