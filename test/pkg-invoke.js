79984410a83f8086101c382f47a01a88163da473
Nathan Rajlich <nathan@tootallnate.net>
2019-03-15 02:08:24 -0700

    Simplify `pkg-invoke.js` code

--- test/pkg-invoke.js
+++ test/pkg-invoke.js
@@ -3,21 +3,22 @@ const { readFileSync } = require('fs');
 const { createFunction } = require('../');
 
 async function main() {
-	const fn = await createFunction({
-		Code: {
-			// `ZipFile` works, or an already unzipped directory may be specified
-			//ZipFile: process.env.HOME + '/lambda.zip'
-			Directory: join(process.cwd(), '/functions/go-echo')
-		},
-		Handler: 'handler',
-		Runtime: 'go1.x'
-	});
-
-	const res = await fn({ hello: 'world' });
-	console.log(JSON.stringify(res));
-	// Prints: { "hello": "world" }
-
-	await fn.destroy();
+	let fn;
+	try {
+		fn = await createFunction({
+			Code: {
+				Directory: join(process.cwd(), '/functions/go-echo')
+			},
+			Handler: 'handler',
+			Runtime: 'go1.x'
+		});
+		const res = await fn({ hello: 'world' });
+		console.log(JSON.stringify(res));
+	} finally {
+		if (fn) {
+			await fn.destroy();
+		}
+	}
 }
 
 main().catch(err => {
