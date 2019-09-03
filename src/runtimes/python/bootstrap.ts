9f21eab72f3f5e8e9cfef92777b09602f4df60ef
Steven <steven@ceriously.com>
2019-09-03 17:36:54 -0400

    Revert changes to python runtime

--- src/runtimes/python/bootstrap.ts
+++ src/runtimes/python/bootstrap.ts
@@ -7,21 +7,5 @@ if (!process.env.PYTHONPATH) {
 	process.env.PYTHONPATH = process.env.LAMBDA_RUNTIME_DIR;
 }
 
-let pythonBin = 'python3';
-const fallback = () => {
-	pythonBin = 'python';
-};
-const child = spawn(pythonBin, ['--version']);
-child.on('error', fallback);
-child.stderr.on('data', fallback);
-child.stdout.on('data', (data?: string) => {
-	const isPython3 =
-		data && data.toString() && data.toString().startsWith('Python 3');
-
-	if (!isPython3) {
-		fallback();
-	}
-
-	const bootstrap = join(__dirname, 'bootstrap.py');
-	spawn(pythonBin, [bootstrap], { stdio: 'inherit' });
-});
+const bootstrap = join(__dirname, 'bootstrap.py');
+spawn('python', [bootstrap], { stdio: 'inherit' });
