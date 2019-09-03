aee8421aa255c89aca0a8a3ee4e79158a3dd07e1
Steven <steven@ceriously.com>
2019-09-03 18:40:20 -0400

    Use stderror with spawn

--- src/runtimes/python3/bootstrap.ts
+++ src/runtimes/python3/bootstrap.ts
@@ -11,9 +11,9 @@ let pythonBin = 'python3';
 const fallback = () => {
 	pythonBin = 'python';
 };
-const child = spawn(pythonBin, ['--version', '2>&1']);
+const child = spawn(pythonBin, ['--version']);
 child.on('error', fallback);
-child.stdout.on('data', (data?: string) => {
+child.stderr.on('data', (data?: string) => {
 	const isPython3 =
 		data && data.toString() && data.toString().startsWith('Python 3');
 
