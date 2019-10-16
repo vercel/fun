b68ede3cf05f0a8e457c93096a867f4945cdd702
Nathan Rajlich <n@n8.io>
2019-10-16 15:16:42 -0700

    Use GH Actions for CI testing (#40)
    * Use GH Actions for CI testing
    
    * Don't install Go
    
    GH Actions already has it installed
    
    * Delete `install-go.sh` script
    
    * Don't test Node 6.x
    
    No support for `async` functions
    
    * Run CI only on "push to master" and "pull requests"

--- .circleci/install-go.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-if [ "$(uname)" = "Darwin" ]; then
-	platform=darwin
-else
-	platform=linux
-fi
-curl -sfLS "https://dl.google.com/go/go1.12.$platform-amd64.tar.gz" | tar zxv --strip-components=1 -C /usr/local
