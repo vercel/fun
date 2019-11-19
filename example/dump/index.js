11f7133d6e317c288c92070d1d0c97d5f84bfd2c
Nathan Rajlich <nathan@tootallnate.net>
2019-11-18 19:54:21 -0800

    Format dump example

--- example/dump/index.js
+++ example/dump/index.js
@@ -1,3 +1,10 @@
-exports.handler = function (event, context) {
-	return { isResponse: true, event, context, env: process.env, versions: process.versions, date: new Date().toString() };
+exports.handler = function(event, context) {
+	return {
+		isResponse: true,
+		event,
+		context,
+		env: process.env,
+		versions: process.versions,
+		date: new Date().toString()
+	};
 };
