6df09e16c85a89238b2c7050db710f5cd4f087f8
Steven <steven@ceriously.com>
2020-01-24 18:46:47 -0500

    Add `RUBYPATH` and `RUBYLIB`

--- src/runtimes/ruby/bootstrap.ts
+++ src/runtimes/ruby/bootstrap.ts
@@ -1,8 +1,11 @@
 import { join } from 'path';
 import { spawn } from 'child_process';
 
-if (!process.env.NOWRUBYPATH) {
-	process.env.NOWRUBYPATH = process.env.LAMBDA_RUNTIME_DIR;
+if (!process.env.RUBYPATH) {
+	process.env.RUBYPATH = process.env.LAMBDA_RUNTIME_DIR;
+}
+if (!process.env.RUBYLIB) {
+	process.env.RUBYLIB = process.env.LAMBDA_RUNTIME_DIR;
 }
 
 const bootstrap = join(__dirname, '..', 'ruby', 'bootstrap.rb');
