e06ab16e8c8c74805b56599177f5854ecdb7c8fa
Nathan Rajlich <nathan@tootallnate.net>
2020-01-24 14:43:32 -0800

    Remove `ruby` bootstrap logic
    It wasn't really doing anything anyways.

--- src/runtimes/ruby/index.ts
+++
@@ -1,6 +0,0 @@
-import { Runtime } from '../../types';
-import { runtimes, initializeRuntime } from '../../runtimes';
-
-export async function init(_runtime: Runtime): Promise<void> {
-	await initializeRuntime(runtimes.ruby);
-}
