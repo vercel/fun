7b74674093161a5340941a4b0339114d4b09ba0a
Eli Front <elifront23@gmail.com>
2025-03-14 20:05:09 -0500

    fix: update uuid import to stay consistent and work with new version

--- src/providers/native/index.ts
+++ src/providers/native/index.ts
@@ -1,5 +1,5 @@
 import ms from 'ms';
-import uuid from 'uuid/v4';
+import { v4 as uuid } from 'uuid';
 import createDebug from 'debug';
 import { promisify } from 'node:util';
 import { AddressInfo } from 'node:net';
